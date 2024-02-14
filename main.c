ee2a9fb230a37779890340e013f523e1f88efd63
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-02-14 12:41:02 +0100

    start of project

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include "player.h"
+#include "enemyStatic.h"
+#include "projectileEnemy.h"
+#include "projectilePlayer.h"
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
